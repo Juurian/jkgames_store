d5b349bb6abf69f7310ee958bb9a6e54ca3be00a
jmacaraeg <jmacaraeg@academic.rrc.ca>
2023-11-30 06:53:55 -0600

    games data is now displaying, will need to get images working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,30 @@
-AdminUser.delete_all
+require "sqlite3"
+require "csv"
+require "active_record"
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+Game.destroy_all
+
+# AdminUser.delete_all
+
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# Specify the file path to your CSV file
+csv_file = Rails.root.join("db/games.csv")
+
+# Loop through the fetched data and seed it into your Rails database
+CSV.foreach(csv_file, headers: true) do |game|
+  Game.create!(
+    title:        game["title"],
+    platform:     game["platform"],
+    description:  game["description"],
+    genre:        game["genre"],
+    price:        game["price"],
+    esrb_rating:  game["esrb_rating"],
+    manufacturer: game["manufacturer"],
+    release_date: game["release_date"],
+    weight:       game["weight"],
+    stock:        game["stock"]
+  )
+end
+
+puts "Seed data created successfully!"
