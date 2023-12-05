639d4b7c390bda0e50a3e4572c85d6e0de169747
Kwanwai Pang <kpang@academic.rrc.ca>
2023-12-04 22:58:45 -0600

    changed the action after adding the cart from redirect to root to stay on current page; added a add to cart button in show.htm.erb of game

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -11,8 +11,11 @@ class CartController < ApplicationController
       game = Game.find(id)
       flash[:notice] = "+ #{game.title} added to cart..."
     end
-
-    redirect_to root_path
+    # redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_back fallback_location: root_path }
+      format.js   { render js: "window.location.reload();" }
+    end
   end
 
 
@@ -25,6 +28,10 @@ class CartController < ApplicationController
     game = Game.find(id)
     flash[:notice] = "- #{game.title} removed from cart..."
     # ToDo - add notification...
-    redirect_to root_path
+    # redirect_to root_path
+    respond_to do |format|
+      format.html { redirect_back fallback_location: root_path }
+      format.js   { render js: "window.location.reload();" }
+    end
   end
 end
