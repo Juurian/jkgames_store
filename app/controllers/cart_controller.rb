3c33b6c9a2dceef33205020dc237ae5b0dbecd64
Kwanwai Pang <kpang@academic.rrc.ca>
2023-12-04 22:39:20 -0600

    can now add item to cart, and then remove it from cart. But no quantity is available yet.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,20 +1,29 @@
 class CartController < ApplicationController
   #POST /cart
+  # POST /cart
   def create
-    # log product id to the terminal logger
+    puts "Create action called!"
     logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
-    session[:shopping_cart] << id unless session[:shopping_cart].include?(id) #pushes id onto the end of array
-    game = Game.find(id)
-    flash[:notice] = "+ #{game.title} added to cart..."
+
+    unless session[:shopping_cart].include?(id)
+      session[:shopping_cart] << id
+      game = Game.find(id)
+      flash[:notice] = "+ #{game.title} added to cart..."
+    end
+
     redirect_to root_path
   end
 
-  #DELETE /cart/:id
+
+  # DELETE /cart/:id
   def destroy
+    puts "Destroy action called!"
     logger.debug("Removing #{params[:id]} from cart.")
     id = params[:id].to_i
     session[:shopping_cart].delete(id)
+    game = Game.find(id)
+    flash[:notice] = "- #{game.title} removed from cart..."
     # ToDo - add notification...
     redirect_to root_path
   end
