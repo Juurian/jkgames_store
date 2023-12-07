4432fb2c151726dd244cd39804f25aa8f1c4e504
Kwanwai Pang <kpang@academic.rrc.ca>
2023-12-07 01:21:18 -0600

    shopping cart with quantity

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,36 +4,61 @@ class CartController < ApplicationController
     puts "Create action called!"
     logger.debug("Adding #{params[:id]} to cart.")
     id = params[:id].to_i
-    price = params[:price].to_f
+    quantity = params[:quantity].to_i
 
-    unless session[:shopping_cart].include?(id)
-      session[:shopping_cart] << id
+    # Ensure the cart is initialized
+    session[:shopping_cart] ||= {}
+
+    # Check if the game is already in the cart
+    if session[:shopping_cart].key?(id)
+      # If the game is already in the cart, update the quantity
+      session[:shopping_cart][id][:quantity] += quantity
+    else
+      # If the game is not in the cart, add it with the specified quantity
       game = Game.find(id)
-      flash[:notice] = "+ #{game.title} added to cart..."
+      session[:shopping_cart][id] = { quantity: quantity, title: game.title, price: game.price }
+      flash[:notice] = "+ #{quantity} #{'game'.pluralize(quantity)} added to cart..."
+      puts "items in session: #{session[:shopping_cart]}"
     end
-    # redirect_to root_path
+
     respond_to do |format|
       format.html { redirect_back fallback_location: root_path }
       format.js   { render js: "window.location.reload();" }
     end
   end
+
+
 
 
   # DELETE /cart/:id
   def destroy
     puts "Destroy action called!"
     logger.debug("Removing #{params[:id]} from cart.")
-    id = params[:id].to_i
-    session[:shopping_cart].delete(id)
-    game = Game.find(id)
-    flash[:notice] = "- #{game.title} removed from cart..."
-    # ToDo - add notification...
-    # redirect_to root_path
+    id = params[:id].to_s
+
+    puts "Before removal: #{session[:shopping_cart].inspect}"
+
+
+    if session[:shopping_cart].key?(id)
+      session[:shopping_cart].delete(id)
+      game = Game.find(id)
+      flash[:notice] = "- #{game.title} removed from cart..."
+
+      # Save the session changes manually
+      session[:shopping_cart] = session[:shopping_cart]
+    else
+      flash[:alert] = "Item not found in cart."
+    end
+
+    puts "After removal: #{session[:shopping_cart].inspect}"
+
     respond_to do |format|
       format.html { redirect_back fallback_location: root_path }
-      format.js   { render js: "window.location.reload();" }
+      format.js { render js: "window.location.reload();" }
     end
   end
+
+
 
   def show
     # Fetch the items from the cart
