03c9531846142e41347c864d15b588775edfe6fb
Julian Macaraeg <92966952+Juurian@users.noreply.github.com>
2023-12-07 03:07:15 -0600

    removed game id from products

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -15,7 +15,6 @@ class GamesController < ApplicationController
 
   # shows the image from active storage
   def show_image
-    game_id = params[:game_id]
     image_name = params[:image_name]
     send_file Rails.root.join('public', 'storage', 'game_master_image', game_id, "#{game_id}.jpg"), disposition: 'inline'
   end
