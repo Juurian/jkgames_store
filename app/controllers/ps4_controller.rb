c40c61d77ea42d5683798e625fc5c105e5f35193
Julian Macaraeg <92966952+Juurian@users.noreply.github.com>
2023-12-07 02:31:38 -0600

    implemented ps4 page and searching within only ps4 games

---
+++ app/controllers/ps4_controller.rb
@@ -0,0 +1,22 @@
+class Ps4Controller < ApplicationController
+  def index
+    @games = Game.where(platform: 'PS4').page(params[:page]).per(24)
+  end
+
+  def show
+    @game = Game.find(params[:id])
+  end
+
+  # renders off of search results
+  def search
+    @games = Game.where(platform: 'PS4').where("title LIKE ?", "%#{params[:search]}%").page(params[:page]).per(10)
+    render 'index'
+  end
+
+  # shows the image from active storage
+  def show_image
+    game_id = params[:game_id]
+    image_name = params[:image_name]
+    send_file Rails.root.join('public', 'storage', 'game_master_image', game_id, "#{game_id}.jpg"), disposition: 'inline'
+  end
+end
