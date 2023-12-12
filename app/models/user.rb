f22ca7bb719e67c153147ab53dcb20ee9fdc497c
Kwanwai Pang <kpang@academic.rrc.ca>
2023-12-12 11:37:30 -0600

    signup page has province but wont send to database

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # Add the province field and validation
+  # validates :province, presence: true
+
+  def self.provinces_list
+    [
+      'Alberta', 'British Columbia', 'Manitoba', 'New Brunswick',
+      'Newfoundland and Labrador', 'Nova Scotia', 'Ontario', 'Prince Edward Island',
+      'Quebec', 'Saskatchewan'
+    ]
+  end
 end
